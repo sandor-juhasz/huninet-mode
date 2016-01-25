6f6d162eac369640521786c8b18226fa6598e13a
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2016-01-25 07:31:47 +0100

    Adding huninet-convert-region.

--- huninet.el
+++ huninet.el
@@ -43,11 +43,40 @@ character is not mapped, it returns the character as is."
       char)))
 
 (defun huninet-mode-convert-region (start end)
-  "Hello, World!")
+  "Converts the selected region to a huninet string."
+  (interactive "*r")
+  (save-excursion
+    (goto-char start)
+    (let ((cnt (- end start))
+	  (i   0))
+      ;(message "%d" cnt)
+      (while (< i cnt)
+	;(message "Executing for i=%d" i)
+	(let* ((p       (point))
+	       (char    (buffer-substring-no-properties p (1+ p)))
+	       (newchar (huninet-convert-char char)))
+	  (delete-char 1)
+	  (insert newchar)
+	  (setq i (1+ i)))))))
 
 ;;
 ;; Unit tests
 ;;
+
+(ert-deftest huninet-convert-region-test ()
+  "Tests if a region is poperly converted."
+  (let ((test-cases '(("a" . "a")
+		      ("árvíztűrő tükörfúrógép" . "a'rvi'ztu\"ro\" tu:ko:rfu'ro'ge'p")
+		      ("ÁRVÍZTŰRŐ TÜKÖRFÚRÓGÉP" . "A'RVI'ZTU\"RO\" TU:KO:RFU'RO'GE'P")
+		      ("a\nb\n" . "a\nb\n"))))
+    (dolist (test-case test-cases)
+      (let ((input-text (car test-case))
+	    (expected-text (cdr test-case)))
+	(message "Test case: \"%s\" -> \"%s\"" input-text expected-text)
+	(with-temp-buffer
+	  (insert input-text)
+	  (huninet-mode-convert-region (point-min) (point-max))
+	  (should (equal (buffer-string) expected-text)))))))
 
 (ert-deftest huninet-convert-char-test ()
   "Tests if a character is properly converted to its huninet counterpart."
@@ -55,6 +84,3 @@ character is not mapped, it returns the character as is."
   (should (equal (huninet-convert-char "Ü") "U:"))
   (should (equal (huninet-convert-char "b") "b")))
 
-(ert-deftest huninet-mode-convert-region-test ()
-  "Tests if the conversion is done properly."
-  (should (equal (huninet-mode-convert-region 10 20) "Hello, World!")))
